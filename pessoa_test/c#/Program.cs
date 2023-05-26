e909eb8984715948e80d95b4a738764ea0fe6041
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-05-26 14:07:39 -0300

    Projetos C# MICROSOFT DYNAMICS

---
+++ pessoa_test/c#/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//dotnet new console - comando para inicializar o arquivo
+
+using c_.Models;
+
+Pessoa pessoa = new Pessoa();
+
+pessoa.Nome = "Lucas";
+pessoa.Idade = 26;
+pessoa.Apresentar();
